4153c1a199c4efd164399f4c177afb55e943b540
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2023-09-01 16:57:10 -0400

    Update ContaminantID_by_Vclustering.py

--- byClustering/ContaminantID_by_Vclustering.py
+++ byClustering/ContaminantID_by_Vclustering.py
@@ -44,7 +44,7 @@ def sort_cluster(HTSfile,idmin ):
 			else:
 				if os.path.isfile('Cluster/cluster_'+cluster+'.fasta'):
 					input2 = open('Cluster/cluster_'+cluster+'.fasta','a+')
-					input2.write('>'+row.split('\t')[9].split('\n')[0]+'\n'+str(fastadict[row.split('\t')[9].split('\n')[0]]) +'\n')
+					input2.write('>'+row.split('\t')[8]+'\n'+str(fastadict[row.split('\t')[8]]) +'\n')
 					input2.close()
 				else:
 					input2 = open('Cluster/cluster_'+cluster+'.fasta','w+')
